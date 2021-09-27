91199ba3e6bc05d7292dc4b3df4764743942ece0
calm0G <72252980+calm0G@users.noreply.github.com>
2021-09-27 14:08:50 +0530

    Update userRepository.java

--- src/main/java/com/Sprint/HealthCareSystem/Repository/userRepository.java
+++ src/main/java/com/Sprint/HealthCareSystem/Repository/userRepository.java
@@ -2,7 +2,7 @@ package com.Sprint.HealthCareSystem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.Sprint.HealthCareSystem.entities.User;
+import com.Sprint.HealthCareSystem.Entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
