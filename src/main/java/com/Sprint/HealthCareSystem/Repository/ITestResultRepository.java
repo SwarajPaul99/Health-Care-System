f2e357a8f8cb155ed4751c263a1469cba5c144d8
calm0G <72252980+calm0G@users.noreply.github.com>
2021-09-27 14:07:30 +0530

    Update ITestResultRepository.java

--- src/main/java/com/Sprint/HealthCareSystem/Repository/ITestResultRepository.java
+++ src/main/java/com/Sprint/HealthCareSystem/Repository/ITestResultRepository.java
@@ -2,7 +2,7 @@ package com.Sprint.HealthCareSystem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.Sprint.HealthCareSystem.entities.TestResult;
+import com.Sprint.HealthCareSystem.Entity.TestResult;
 
 @Repository
 public interface ITestResultRepository extends JpaRepository<TestResult, Integer> {
