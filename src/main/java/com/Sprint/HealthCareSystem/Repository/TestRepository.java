2450eca42e9d899d18f2b0c92294bff9c731792b
calm0G <72252980+calm0G@users.noreply.github.com>
2021-09-27 14:07:57 +0530

    Update TestRepository.java

--- src/main/java/com/Sprint/HealthCareSystem/Repository/TestRepository.java
+++ src/main/java/com/Sprint/HealthCareSystem/Repository/TestRepository.java
@@ -2,7 +2,7 @@ package com.Sprint.HealthCareSystem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.Sprint.HealthCareSystem.entities.DiagnosticTest;
+import com.Sprint.HealthCareSystem.Entity.DiagnosticTest;
 
 @Repository
 public interface TestRepository extends JpaRepository<DiagnosticTest, Integer>{
