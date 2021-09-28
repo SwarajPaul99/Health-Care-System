96b714b4e98016c7a167c2f50beed72bafbeb2aa
Taufikur Rahaman <89567640+TRAHAMAN@users.noreply.github.com>
2021-09-28 15:17:31 +0530

    Add files via upload

---
+++ src/main/java/com/Sprint/HealthCareSystem/Repository/IDiagnosticCenterRepository.java
@@ -0,0 +1,12 @@
+package com.Sprint.HealthCareSystem.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Sprint.HealthCareSystem.Entity.DiagnosticCenter;
+
+
+@Repository
+public interface IDiagnosticCenterRepository extends JpaRepository<DiagnosticCenter, Integer>{
+
+}
